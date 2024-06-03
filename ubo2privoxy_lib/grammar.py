fd34e759d4519e6d3f231ae91fb0f3d18bc9652e
readysloth <readysloth@protonmail.com>
2024-06-03 22:01:57 +0300

    fix(ubo2privoxy_lib/grammar.py): Made grammar more flexible

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -8,7 +8,7 @@ MATCH_DOMAIN: "||"
 ELEM_HIDING_MARK: /#.?#/
 ELEM_HIDING: ELEM_HIDING_MARK /[^\r\n]*/
 FILTER_OPT: /[^,\s]*/
-BLOCKING_PATTERN: /([^^@|$\r\n])+/
+BLOCKING_PATTERN: /[^^|$\r\n]+/
 FILTER_OPTS: FILTER_OPT_START (FILTER_OPT ","?)*
 
 SKIP_PATTERNS: ELEM_HIDING_MARK | /^~/
@@ -20,7 +20,10 @@ _NEWLINE.9999: ( _EOL | COMMENT )+
 
 blocking_pattern: SEPARATOR? (BLOCKING_PATTERN SEPARATOR?)+
 rule_opt: (ANCHOR | EXCEPTION | MATCH_DOMAIN)~0..2
-filter_line: _NEWLINE | FILTER_OPTS | rule_opt? blocking_pattern FILTER_OPTS?
+filter_line: _NEWLINE
+           | FILTER_OPTS
+           | rule_opt? blocking_pattern FILTER_OPTS?
+           | rule_opt? blocking_pattern? FILTER_OPTS
 filter_file: filter_line*
 
 ?start: filter_file
