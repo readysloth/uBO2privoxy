a11c74e5e520155aa23c0d02d4396d4813df646a
readysloth <readysloth@protonmail.com>
2024-06-04 10:30:02 +0300

    feat(ubo2privoxy_lib/grammar.py): Added regex patterns recongition
    Fixed url matching with more precise regex

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -4,11 +4,14 @@ EXCEPTION: "@@"
 FILTER_OPT_START: "$"
 SEPARATOR: "^"
 MATCH_DOMAIN: "||"
+REGEX_MARK: "/"
 
+REGEX_BODY: /(\\\/|[^\/])+/
 ELEM_HIDING_MARK: /#.?#/
 ELEM_HIDING: ELEM_HIDING_MARK /[^\r\n]*/
 FILTER_OPT: /[^,\s]*/
-BLOCKING_PATTERN: /[^^|$\r\n]+/
+VALID_URL: /[-a-zA-Z0-9@:%._\+~#=\/*?&]+/
+BLOCKING_PATTERN: VALID_URL
 FILTER_OPTS: FILTER_OPT_START (FILTER_OPT ","?)*
 
 SKIP_PATTERNS: ELEM_HIDING_MARK | /^~/
@@ -17,13 +20,13 @@ COMMENT: "!" /[^\n]*/
 _EOL: /[\t ]*\r?\n/
 _NEWLINE.9999: ( _EOL | COMMENT )+
 
-
-blocking_pattern: SEPARATOR? (BLOCKING_PATTERN SEPARATOR?)+
+regex: REGEX_MARK REGEX_BODY REGEX_MARK
+blocking_pattern: regex
+                | SEPARATOR? (BLOCKING_PATTERN SEPARATOR?)+
 rule_opt: (ANCHOR | EXCEPTION | MATCH_DOMAIN)~0..2
 filter_line: _NEWLINE
-           | FILTER_OPTS
+           | rule_opt? FILTER_OPTS
            | rule_opt? blocking_pattern FILTER_OPTS?
-           | rule_opt? blocking_pattern? FILTER_OPTS
 filter_file: filter_line*
 
 ?start: filter_file
