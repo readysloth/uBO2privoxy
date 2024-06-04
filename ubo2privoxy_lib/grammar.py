592f41511d37186ed42e29ccb6faa90f426f9b8a
readysloth <readysloth@protonmail.com>
2024-06-04 12:05:29 +0300

    fix(ubo2privoxy_lib/grammar.py): Lowered precedence of `BLOCKING_PATTERN`
    Fixed `VALID_URL` regex

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -10,15 +10,15 @@ REGEX_BODY: /(\\\/|[^\/])+/
 ELEM_HIDING_MARK: /#.?#/
 ELEM_HIDING: ELEM_HIDING_MARK /[^\r\n]*/
 FILTER_OPT: /[^,\s]*/
-VALID_URL: /[-a-zA-Z0-9@:%._\+~#=\/*?&;]+/
-BLOCKING_PATTERN: VALID_URL
+VALID_URL: /[-a-zA-Z0-9@:%._\+~#=\/*?&;()]+/
+BLOCKING_PATTERN.-1000: VALID_URL
 FILTER_OPTS: FILTER_OPT_START (FILTER_OPT ","?)*
 
 SKIP_PATTERNS: ELEM_HIDING_MARK | /^~/
 SKIP: /[^\n]*/ SKIP_PATTERNS /[^\n]*/
 COMMENT: "!" /[^\n]*/
 _EOL: /[\t ]*\r?\n/
-_NEWLINE.9999: ( _EOL | COMMENT | SKIP)+
+_NEWLINE: ( _EOL | COMMENT | SKIP)+
 
 regex: REGEX_MARK REGEX_BODY REGEX_MARK
 blocking_pattern: regex
