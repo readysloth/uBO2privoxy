d1a7daa2e7d39c0304f03a98c8e7e23dd64fc991
readysloth <readysloth@protonmail.com>
2024-06-07 22:51:05 +0300

    feat(ubo2privoxy_lib/grammar.py): Modified grammar, so now it doesn't crash on UBO rules

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -7,29 +7,33 @@ IGNORE: (COMMENT_LINE | INLINE_COMMENT)? NEWLINE
 %ignore IGNORE
 
 NOT: "~"
-ANCHOR: "||"
+ANCHOR: "|"
+DOMAIN_ANCHOR: "||"
 EXCEPTION: "@@"
 SEPARATOR_PLACEHOLDER: "^"
-NET_ANCHOR: "$"
+
+// Not accurate, but filters are not yet supported
+NET_ANCHOR: "$" | "#$"
 LOCALHOST: "127.0.0.1" | "localhost" | "local"
 THISHOST: "0.0.0.0" | "broadcasthost"
 
 REST_OF_LINE: /[^\r\n]+/
 EXT_ANCHOR: /(#@?(?:\$\?|\$|%|\?)?#).{1,2}/
 FILTER_OPT: NET_ANCHOR /.*/
-PLAIN_GENERIC_COSMETIC: "##" REST_OF_LINE
+PLAIN_GENERIC_COSMETIC: /#.?#/ REST_OF_LINE
 
-PATH: /[0-9a-z%&,\-.\/:;=?_]+/
-ANCHORED_PATH: /^[0-9a-z%&,\-.\/:;=?_]+/
+DOMAIN: /[0-9a-zA-Z_.]+/
+PATH: /[^~|$][0-9a-zA-Z%&,\-.\/:;=?_~]*/
+TEMPLATE_PATH: /[^~|$][0-9a-zA-Z%&,\-.\/:;=?_~*]*/
 PROTOCOL: /https?/
 PROTOCOL_SEPARATOR: "://"
 
 hosts: (LOCALHOST | THISHOST) " " PATH
-url: PROTOCOL? PROTOCOL_SEPARATOR REST_OF_LINE
-   | ANCHORED_PATH SEPARATOR_PLACEHOLDER? FILTER_OPT?
-path_rule: EXCEPTION? ANCHOR PATH SEPARATOR_PLACEHOLDER? FILTER_OPT?
-         | url
-cosmetic: (NOT? PATH ","?)* PLAIN_GENERIC_COSMETIC
+url: ANCHOR? SEPARATOR_PLACEHOLDER? TEMPLATE_PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
+path_rule: EXCEPTION? DOMAIN_ANCHOR PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
+         | EXCEPTION? (PROTOCOL? PROTOCOL_SEPARATOR)? url
+         | FILTER_OPT
+cosmetic: (NOT? DOMAIN ","?)* PLAIN_GENERIC_COSMETIC
 
 ubo_rule: hosts
         | cosmetic
