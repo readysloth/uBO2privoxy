9b0c0f0efc82bcbd27a0626ba7363cb0d0810b92
readysloth <readysloth@protonmail.com>
2024-06-08 14:27:28 +0300

    fix(ubo2privoxy_lib/grammar.py): More complex grammar with some false-matches resolved

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -1,9 +1,12 @@
 UBO_GRAMMAR = r'''
 
-COMMENT_LINE: /(?:!|#\s|####|\[adblock).*/i
-INLINE_COMMENT: /(?:\s+#).*?$/
+COMMENT_LINE: /(!|#\s|####|\[adblock).*/i
+INLINE_COMMENT: /(\s+#).*?$/
 NEWLINE: /[\r\n]/
+COND_IF: /!#if/
+COND_ENDIF: /!#endif/
 IGNORE: (COMMENT_LINE | INLINE_COMMENT)? NEWLINE
+      | COND_IF /[^!]+/ COND_ENDIF NEWLINE
 
 NOT: "~"
 ANCHOR: "|"
@@ -17,25 +20,24 @@ LOCALHOST: "127.0.0.1" | "localhost" | "local"
 THISHOST: "0.0.0.0" | "broadcasthost"
 
 REST_OF_LINE: /[^\r\n]+/
-EXT_ANCHOR: /(#@?(?:\$\?|\$|%|\?)?#).{1,2}/
+EXT_ANCHOR: /(#@?(\$\?|\$|%|\?)?#).{1,2}/
 FILTER_OPT: NET_ANCHOR /.*/
 PLAIN_GENERIC_COSMETIC: /#.?#/ REST_OF_LINE
 
-DOMAIN: /[0-9a-zA-Z_.\-]+/
-PATH: /[^^#~|$][0-9a-zA-Z%&,\-.\/:;=?_~]*/
-TEMPLATE_PATH: /[^^#~|$][0-9a-zA-Z%&,\-.\/:;=?_~*]*/
+DOMAIN: /[0-9a-zA-Z_.\-*]+/
+PATH: /([^@^#~|$][0-9a-zA-Z%&,\-.\/:;=?_~*]*)(?!.*##)/
 PROTOCOL: /https?/
 PROTOCOL_SEPARATOR: "://"
 
 hosts: (LOCALHOST | THISHOST) " " PATH
-url: ANCHOR? SEPARATOR_PLACEHOLDER? TEMPLATE_PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
-path_rule: EXCEPTION? DOMAIN_ANCHOR PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
-         | EXCEPTION? (PROTOCOL? PROTOCOL_SEPARATOR)? url
+url: ANCHOR? SEPARATOR_PLACEHOLDER? PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
+path_rule: DOMAIN_ANCHOR PATH SEPARATOR_PLACEHOLDER? ANCHOR? FILTER_OPT?
+         | (PROTOCOL? PROTOCOL_SEPARATOR)? url
          | FILTER_OPT
 cosmetic: (NOT? DOMAIN ","?)* PLAIN_GENERIC_COSMETIC
 
 ubo_rule: hosts
         | cosmetic
         | path_rule
-start: (ubo_rule | IGNORE)*
+start: (EXCEPTION? ubo_rule | IGNORE)*
 '''
