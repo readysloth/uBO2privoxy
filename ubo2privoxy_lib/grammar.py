b1ef24b41f01b7813e501736cdff6955952f55ae
readysloth <readysloth@protonmail.com>
2024-06-08 10:36:51 +0300

    fix(ubo2privoxy_lib/grammar.py): Corrected PATH's terminals for correct parsing
    But now some edge cases do fail

--- ubo2privoxy_lib/grammar.py
+++ ubo2privoxy_lib/grammar.py
@@ -22,8 +22,8 @@ FILTER_OPT: NET_ANCHOR /.*/
 PLAIN_GENERIC_COSMETIC: /#.?#/ REST_OF_LINE
 
 DOMAIN: /[0-9a-zA-Z_.\-]+/
-PATH: /[^~|$][0-9a-zA-Z%&,\-.\/:;=?_~]*/
-TEMPLATE_PATH: /[^~|$][0-9a-zA-Z%&,\-.\/:;=?_~*]*/
+PATH: /[^^#~|$][0-9a-zA-Z%&,\-.\/:;=?_~]*/
+TEMPLATE_PATH: /[^^#~|$][0-9a-zA-Z%&,\-.\/:;=?_~*]*/
 PROTOCOL: /https?/
 PROTOCOL_SEPARATOR: "://"
 
